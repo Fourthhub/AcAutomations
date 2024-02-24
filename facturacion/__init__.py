81adfa216b91c1b0ae56b655790d8d0b41d5e94c
diego <diegoechaure@gmail.com>
2024-02-24 16:45:04 +0100

    dale pa

--- facturacion/__init__.py
+++ facturacion/__init__.py
@@ -1,7 +1,36 @@
+import datetime
 import logging
 from holdedModule import crearFactura
 import azure.functions as func
 import json  # Importa el módulo json para procesar el cuerpo de la solicitud
+
+def crearFactura(reserva):
+    # Convertir la fecha de reserva a timestamp
+    
+    now = datetime.datetime.now()
+    timestamp = int(datetime.datetime.timestamp(now))
+
+    payload = {
+        "applyContactDefaults": True,
+        "items": [
+            {
+                "name": f"{reserva['listingName']} - {reserva['arrivalDate']} a {reserva['departureDate']}",
+                "subtotal": str(reserva["totalPrice"])
+            }
+        ],
+        "currency": reserva["currency"],
+        "date": timestamp,  # Uso de timestamp de la fecha de reserva
+        #"numSerieId": "Rocio",
+        "contactName": reserva["guestName"]  # Uso del nombre del huésped
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "key": "260f9570fed89b95c28916dee27bc684"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    return response.status_code
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
