679a27b8f1b12bcb74fd5a11a9db755b5f1c128a
diego <diegoechaure@gmail.com>
2024-02-24 16:34:06 +0100

    few

--- facturacion/__init__.py
+++ facturacion/__init__.py
@@ -1,5 +1,5 @@
 import logging
-import holdedModule
+from holdedModule import crearFactura
 import azure.functions as func
 import json  # Importa el módulo json para procesar el cuerpo de la solicitud
 
